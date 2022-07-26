553c6ee4eb686d57f7af8ebd59998be0856bc168
Jake Hammond <jakehammond226@gmail.com>
2022-07-26 10:43:29 -0500

    done

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+namespace PalindromeExercise;
+
+
+public class WordSmith
+{
+    public WordSmith()
+    {
+    }
+
+    public bool IsAPalindrome(string word)
+    {
+        return word.ToLower().SequenceEqual(word.ToLower().Reverse());
+
+        //string backwards = "";
+
+        //for (int i = word.Length - 1; i >= 0; i--)
+        //{
+        //    backwards += word[i]; 
+        //}
+        //if (word == backwards)
+        //{
+        //    return true;
+        //}
+        //else
+        //{
+        //    return false; 
+        //}
+        
+    }
+
+    
+}
+
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,13 +1,30 @@
 using System;
 using Xunit;
+using PalindromeExercise; 
+
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("dad", true)]
+        [InlineData("Hannah", true)]
+        [InlineData("Levels",false)]
+
+        public void Test1(string word, bool expected)
         {
+
+            //Arrange
+            var wordSmith = new WordSmith();
+
+            //Act
+            var actual = wordSmith.IsAPalindrome(word); 
+
+            //Assert
+            Assert.Equal(expected, actual);
+
 
         }
     }
